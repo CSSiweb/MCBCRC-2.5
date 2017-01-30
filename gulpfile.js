c5dbab8a3e51e9b8ed28eeec7f09f0dfccd9cb6e
Clara <seehuesdesign@gmail.com>
2017-01-30 08:19:23 -0500

    exercize files from 1-29-2017

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,4 @@
-var gulp = require('gulp'), 
-watch = require('gulp-watch');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-	console.log("Hooray - you created a Gulp task.");	
-});
 
-gulp.task('html', function() {
-	console.log("Imagine something useful being done to your HTML here.");
-});
-
-gulp.task('styles', function() {
-	return gulp.scr('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function() {
-	
-	watch('./app/index.html', function() {
-		gulp.start('html');
-	});
-	
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
-	});
-	
-});
